7ef826ec0fbc4511e122f18e4b3e942047aa0bf3
Aman Patel <106889345+Amanpatel2529@users.noreply.github.com>
2022-12-08 12:49:16 +0530

    Add files via upload

---
+++ Lec8.1-DOM_select/script.js
@@ -0,0 +1,20 @@
+//* DOM -> Document Object Model
+
+//The HTML DOM is an Object Model for HTML. It defines:
+//* HTML elements as objects
+//* Properties for all HTML elements
+//* Methods for all HTML elements
+//* Events for all HTML elements
+    
+//The HTML DOM is an API (Programming Interface) for JavaScript:
+//* JavaScript can add/change/remove HTML elements
+//* JavaScript can add/change/remove HTML attributes
+//* JavaScript can add/change/remove CSS styles
+//* JavaScript can react to HTML events
+//* JavaScript can add/change/remove HTML events
+
+document  //entire page
+document.body  //entire body 
+document.getElementById('btn')  //gets element with id 'btn'
+document.getElementsByTagName('p')  //gets element with tag 'p'
+document.getElementsByClassName('para')  //gets element with class 'para'
